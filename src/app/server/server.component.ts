e7206557e450531731f6868adc2f782d6a3bd2e8
kuntal <deisiduke1986@gmail.com>
2022-03-27 21:18:57 +0530

    The basics

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,17 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
 
   serverId = 10;
-  serverStatus = 'offline';
+  serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   getServerStatus() {
     return this.serverStatus;
   }
-  // constructor() { }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+
 
   // ngOnInit(): void {
   // }
