c30a905f1cb9e733488bdf6d716b01fb74db299b
Kuntal Banik <deisiduke1986@gmail.com>
2022-03-28 16:16:10 +0530

    commit updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng4-complete-guide';
+  secretPassword = false;
+  log:any = [];
+
+  onToggleDetails() {
+    
+    this.secretPassword = !this.secretPassword;
+    this.log.push(new Date());
+  }
 }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
+  // Also we can use selector like below
+  // selector: '[app-servers]',
+  // 
+  // In html it will be 
+  // <div app-servers></div>
+  // 
+  // 
+  // or we can use this like below
+  // selector: '.app-servers',
+  // 
+  // In html it will be
+  // <div class="app-servers"></div>
+  // 
   templateUrl: './server.component.html',
+  // or we can use the template with lesser code
+  // template: `
+  //          <app-server></app-server>
+  //          <app-server2></app-server2>`,
+  // 
+  // styleUrls: ['./server.component.css']
+  // 
+  // or we can use styles as shown below
+  // styles: [`
+  // h3 {
+  //    color: blue;
+  // }
+  // `] 
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
